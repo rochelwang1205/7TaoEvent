27c6259a4b6a0c97228b60918f529f8b6b379bfc
Rochel <liuyingfeng57@gmail.com>
2023-08-24 19:04:30 +0800

    修改nav、header結構

--- main.js
+++ main.js
@@ -2,26 +2,26 @@ import './assets/scss/all.scss';
 import 'bootstrap/dist/js/bootstrap.min.js';
 
 //index album line
-const swiper = new Swiper(".mySwiper", {
+// const swiper = new Swiper(".mySwiper", {
   
-    //避免使用者移動到
-    allowTouchMove: false,
+//     //避免使用者移動到
+//     allowTouchMove: false,
     
-    //能呈現幾個.swiper-slide
-    slidesPerView: "auto",
+//     //能呈現幾個.swiper-slide
+//     slidesPerView: "auto",
       
-    //跑馬燈的速度(毫秒)
-    speed: 5000,
+//     //跑馬燈的速度(毫秒)
+//     speed: 5000,
     
-    //是否重複
-    loop: true,
+//     //是否重複
+//     loop: true,
     
-    autoplay: {
+//     autoplay: {
       
-      //到下一個幻燈片時，停下的時間
-      delay: 0,    
+//       //到下一個幻燈片時，停下的時間
+//       delay: 0,    
       
-      //避免點擊造成跑馬燈失效，視情況增加
-      disableOnInteraction: false
-    }
-  });
+//       //避免點擊造成跑馬燈失效，視情況增加
+//       disableOnInteraction: false
+//     }
+//   });
